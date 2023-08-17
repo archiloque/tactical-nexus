783114eceb94a7b51773b77821444293df7892b4
Julien Kirch <code@archiloque.net>
2023-08-17 23:49:12 +0200

    no message

--- tactical-nexus-solver/src/main/kotlin/net/archiloque/tacticalnexus/solver/code/PlayableTower.kt
+++ tactical-nexus-solver/src/main/kotlin/net/archiloque/tacticalnexus/solver/code/PlayableTower.kt
@@ -3,6 +3,7 @@ package net.archiloque.tacticalnexus.solver.code
 import net.archiloque.tacticalnexus.solver.Position
 import net.archiloque.tacticalnexus.solver.entities.Entity
 import net.archiloque.tacticalnexus.solver.entities.Level
+import net.archiloque.tacticalnexus.solver.entities.Staircase
 import net.archiloque.tacticalnexus.solver.entities.Tower
 import net.archiloque.tacticalnexus.solver.entities.Wall
 
@@ -12,20 +13,37 @@ class PlayableTower(
     val entitiesIndexByPosition: Map<Position, Int>,
     val startingPosition: Int,
     val reachableEntities: Array<Array<Int>>,
+    val stairs: Map<Int, Int>,
 ) {
 
     companion object {
         fun prepare(tower: Tower): PlayableTower {
             val positionedEntitiesList = mutableListOf<PositionedEntity>()
             var startingPosition: Int = -1
+            val upStaircases = mutableMapOf<Int, Int>()
+            val downStaircases = mutableMapOf<Int, Int>()
+
             tower.levels().forEachIndexed { levelIndex, level ->
                 for (line in 0..<level.lines) {
                     for (column in 0..<level.columns) {
                         val entity = level.entities[line][column]
                         if ((entity != null) && (entity.getType() != Entity.EntityType.Wall)) {
                             val positionedEntity = PositionedEntity(entity, levelIndex, line, column)
+                            val currentEntityIndex = positionedEntitiesList.size
+
+                            if (entity.getType() == Entity.EntityType.Staircase) {
+                                when ((entity as Staircase).direction) {
+                                    Staircase.StaircaseDirection.up -> {
+                                        upStaircases[levelIndex] = currentEntityIndex
+                                    }
+                                    Staircase.StaircaseDirection.down -> {
+                                        downStaircases[levelIndex] = currentEntityIndex
+                                    }
+                                }
+                            }
+
                             if (entity.getType() == Entity.EntityType.PlayerStartPosition) {
-                                startingPosition = positionedEntitiesList.size
+                                startingPosition = currentEntityIndex
                             }
                             positionedEntitiesList.add(positionedEntity)
                         }
@@ -48,12 +66,17 @@ class PlayableTower(
                     entitiesIndexByPosition
                 )
             }.toTypedArray()
+            val stairs = mutableMapOf<Int, Int>()
+            upStaircases.forEach { levelIndex, entityIndex ->
+                stairs[entityIndex] = downStaircases[levelIndex + 1]!!
+            }
             return PlayableTower(
                 positionedEntities.size,
                 positionedEntities,
                 entitiesIndexByPosition,
                 startingPosition,
                 reachableEntities,
+                stairs,
             )
         }
 
--- tactical-nexus-solver/src/main/kotlin/net/archiloque/tacticalnexus/solver/entities/Staircase.kt
+++ tactical-nexus-solver/src/main/kotlin/net/archiloque/tacticalnexus/solver/entities/Staircase.kt
@@ -1,7 +1,7 @@
 package net.archiloque.tacticalnexus.solver.entities
 
-import net.archiloque.tacticalnexus.solver.code.StateSaver
 import net.archiloque.tacticalnexus.solver.code.PlayableTower
+import net.archiloque.tacticalnexus.solver.code.StateSaver
 import net.archiloque.tacticalnexus.solver.database.State
 
 data class Staircase(val direction: StaircaseDirection) : Entity() {
@@ -15,7 +15,20 @@ data class Staircase(val direction: StaircaseDirection) : Entity() {
     }
 
     override fun play(entityIndex: Int, state: State, playableTower: PlayableTower, stateSaver: StateSaver) {
-        TODO("Not yet implemented")
+        when(direction) {
+            StaircaseDirection.up -> {
+                val matchingDownStaircase = playableTower.stairs[entityIndex]!!
+                val newPosition = newState(entityIndex, state)
+                newPosition.visitedEntities.set(matchingDownStaircase)
+                for(entityReachableOnNextLevel in playableTower.reachableEntities[matchingDownStaircase]) {
+                    newPosition.reachableEntities.set(entityReachableOnNextLevel)
+                }
+                addNewPositions(entityIndex, newPosition, playableTower, stateSaver)
+            }
+            StaircaseDirection.down -> {
+                throw IllegalStateException("Should not happen")
+            }
+        }
     }
 
     enum class StaircaseDirection() {
