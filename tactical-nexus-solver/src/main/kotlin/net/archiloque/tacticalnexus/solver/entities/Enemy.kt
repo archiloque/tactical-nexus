b6abc5f6fc1e18dc5c01d5e10036edfaa6c4bd36
Julien Kirch <code@archiloque.net>
2023-08-17 23:02:42 +0200

    no message

--- tactical-nexus-solver/src/main/kotlin/net/archiloque/tacticalnexus/solver/entities/Enemy.kt
+++ tactical-nexus-solver/src/main/kotlin/net/archiloque/tacticalnexus/solver/entities/Enemy.kt
@@ -1,7 +1,7 @@
 package net.archiloque.tacticalnexus.solver.entities
 
-import net.archiloque.tacticalnexus.solver.code.StateSaver
 import net.archiloque.tacticalnexus.solver.code.PlayableTower
+import net.archiloque.tacticalnexus.solver.code.StateSaver
 import net.archiloque.tacticalnexus.solver.database.State
 
 data class Enemy(
@@ -17,7 +17,46 @@ data class Enemy(
 
     override fun play(entityIndex: Int, state: State, playableTower: PlayableTower, stateSaver: StateSaver) {
         val newPosition = newState(entityIndex, state)
-        drop.collect(newPosition)
-        addNewPositions(entityIndex, newPosition, playableTower, stateSaver)
+        val fightResult = fight(
+            state.atk,
+            state.def,
+            state.hp,
+            atk,
+            def,
+            hp
+        )
+        if (fightResult != null) {
+            newPosition.hp = fightResult
+            drop.collect(newPosition)
+            addNewPositions(entityIndex, newPosition, playableTower, stateSaver)
+        }
+    }
+
+    companion object {
+        fun fight(playerAtk: Int, playerDef: Int, playerHp: Int, enemyAtk: Int, enemyDef: Int, enemyHp: Int): Int? {
+            var damagesToEnemy = Math.max(playerAtk - enemyDef, 0)
+            var damagesToPlayer = Math.max(enemyAtk - playerDef, 0)
+            if ((damagesToPlayer == 0) && (damagesToEnemy > 0)) {
+                // Enemy can't hurt player but player can hurt enemy
+                return playerHp
+            } else if (damagesToEnemy == 0) {
+                // Can't hurt enemy
+                return null
+            } else {
+                var playerCurrentHp = playerHp
+                var enemyCurrentHp = enemyHp
+                for (turn in 0..1000) {
+                    enemyCurrentHp -= damagesToEnemy
+                    if (enemyCurrentHp <= 0) {
+                        return playerCurrentHp
+                    }
+                    playerCurrentHp -= damagesToPlayer
+                    if (playerCurrentHp <= 0) {
+                        return null
+                    }
+                }
+                throw RuntimeException("Too many turns: ${playerAtk} ${playerDef} ${playerHp} ${enemyAtk} ${enemyDef} ${enemyHp}")
+            }
+        }
     }
 }
